e15e63a43e91da2bca7936111758af2fde7ff665
jlegacy <jlegacy@suddenlink.net>
2014-04-26 23:08:34 -0400

    Splash Screen Working

--- gen/pkg/example/airductcalculator/R.java
+++ gen/pkg/example/airductcalculator/R.java
@@ -25,15 +25,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int customspinner=0x7f020000;
-        public static final int downarrow=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int myspinneritem=0x7f020003;
-        public static final int outputfields=0x7f020004;
-        public static final int round=0x7f020005;
+        public static final int custom_ems=0x7f020000;
+        public static final int customspinner=0x7f020001;
+        public static final int downarrow=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int myspinneritem=0x7f020004;
+        public static final int outputfields=0x7f020005;
+        public static final int round=0x7f020006;
     }
     public static final class id {
         public static final int Container=0x7f090000;
+        public static final int ImperialButton=0x7f09001e;
+        public static final int MetricButton=0x7f09001f;
         public static final int TextDuctSizeBlank=0x7f090013;
         public static final int TextView04=0x7f090001;
         public static final int TextView1=0x7f090003;
@@ -44,12 +47,14 @@ public final class R {
         public static final int TextViewDuctSize=0x7f090011;
         public static final int TextViewVelocity1=0x7f09000d;
         public static final int TextViewVelocity2=0x7f09000f;
-        public static final int action_settings=0x7f09001c;
+        public static final int action_settings=0x7f090020;
+        public static final int container=0x7f09001c;
         public static final int ductTypeSpinner=0x7f090002;
         public static final int editCFM=0x7f09000a;
         public static final int editFriction=0x7f090006;
         public static final int editFriction2=0x7f090017;
         public static final int editVelocity=0x7f09000e;
+        public static final int imageView1=0x7f09001d;
         public static final int squareRound=0x7f090014;
         public static final int squareRoundData=0x7f090018;
         public static final int squareRoundDataBlank=0x7f090019;
@@ -65,9 +70,12 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_main_splash=0x7f030001;
+        public static final int fragment_main_splash=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
+        public static final int main_splash=0x7f080001;
     }
     public static final class string {
         public static final int _05=0x7f05000c;
@@ -97,12 +105,16 @@ public final class R {
         public static final int height_you_wish_duct_to_be=0x7f050021;
         public static final int hello_world=0x7f050001;
         public static final int if_the_velocity_is_greater_than_700_fpm_for_flex_duct_or_900_fpm_for_metal_duct_then_enlarge_duct_by_one_size_all_return_ducts_should_be_limited_to_700_fpm_=0x7f05001d;
+        public static final int imperial=0x7f050026;
         public static final int imperial_units=0x7f050016;
         public static final int in_wc_100ft=0x7f05001a;
+        public static final int metric=0x7f050027;
         public static final int oddColor=0x7f050009;
         public static final int round_duct_size=0x7f050014;
         public static final int small_text=0x7f050006;
         public static final int titleColor=0x7f05000a;
+        public static final int title_activity_main_splash=0x7f050025;
+        public static final int todo=0x7f050028;
         public static final int use_this_rate_to_size_entire_duct_system=0x7f050018;
         public static final int velocity_fpm_=0x7f05000b;
         public static final int velocity_of_air=0x7f05001c;
---
+++ src/pkg/example/airductcalculator/MainSplash.java
@@ -0,0 +1,95 @@
+package pkg.example.airductcalculator;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainSplash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_splash);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		addListenerOnButton();
+		
+	}
+	
+	public void addListenerOnButton() {
+		
+		final Context context = this;
+		
+		Button ImperialButton  = (Button) findViewById(R.id.ImperialButton);
+		Button MetricButton  = (Button) findViewById(R.id.MetricButton);
+		
+		ImperialButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				 Intent intent = new Intent(context, MainActivity.class);
+                 startActivity(intent);   
+
+			}
+
+		});
+		
+		MetricButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+
+				//CalculateResults('U');
+
+			}
+
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_splash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main_splash,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
